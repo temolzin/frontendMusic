b77effa4e4634186004ddd0570e2f0b90f71d9f2
AlbertoGuzman <2519160038jguzmang@gmail.com>
2022-03-15 14:54:04 -0600

    Adding loading screen and google image fix

--- src/pages/Auth/LoginGoogle.vue
+++ src/pages/Auth/LoginGoogle.vue
@@ -1,32 +1,69 @@
-<template>
-  <h4>Login with Gmail...</h4>
-</template>
+<template></template>
 <script>
 import { useQuasar } from "quasar";
 import { mapActions, mapGetters } from "vuex";
-import { ref } from "vue";
+
+import { QSpinnerFacebook } from "quasar";
+import { onBeforeUnmount } from "vue";
 
 let $q;
 export default {
   name: "LoginGoogle",
 
+  setup() {
+    const $q = useQuasar();
+    let timer;
+
+    onBeforeUnmount(() => {
+      if (timer !== void 0) {
+        clearTimeout(timer);
+        $q.loading.hide();
+      }
+    });
+
+    return {
+      showLoading() {
+        $q.loading.show({
+          spinner: QSpinnerFacebook,
+          spinnerColor: "accent",
+          spinnerSize: 140,
+          backgroundColor: "primary",
+          message: "Redirigiendo...",
+          messageColor: "white",
+        });
+
+        // hiding in 3s
+        timer = setTimeout(() => {
+          $q.loading.hide();
+          timer = void 0;
+        }, 3000);
+      },
+    };
+  },
   methods: {
     ...mapActions("auth", ["doLoginGmailCallback"]),
     loginUser() {
       try {
         this.doLoginGmailCallback({
           code: this.$route.query.code,
-        }).then((response) => {
-          $q.notify({
-            type: "positive",
-            message: this.getToken.acction,
+        })
+          .then(() => {
+            $q.notify({
+              type: "positive",
+              message: this.getToken.acction,
+            });
+            this.$router.push({ name: "dashboardH" });
+          })
+          .catch((err) => {
+            $q.notify({
+              type: "negative",
+              message: err.message,
+            });
+            const toPath = this.$route.query.to || "/login";
+            this.$router.push(toPath);
           });
-
-          const toPath = this.$route.query.to || "/dashboard/home";
-          this.$router.push(toPath);
-        });
       } catch (err) {
-        console.log("error");
+        //console.log("error");
       }
     },
   },
@@ -36,6 +73,7 @@ export default {
   created() {
     $q = useQuasar();
     this.loginUser();
+    this.showLoading();
   },
 };
 </script>
