1c0522cc5233e2a7e6b0b3f5771db0f8bcd705f1
SJAmanda <amandajacobo10@gmail.com>
2026-08-10 16:24:41 -0600

    Fix client artist gallery carousel not auto-advancing

--- src/pages/Client/MusicalGenders/show.vue
+++ src/pages/Client/MusicalGenders/show.vue
@@ -255,6 +255,7 @@
             infinite
             v-model="slide"
             v-model:fullscreen="fullscreen"
+            :autoplay="imageList.length > 1 ? 8000 : false"
             arrows
             transition-prev="slide-right"
             transition-next="slide-left"
